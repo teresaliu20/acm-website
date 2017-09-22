e7d70a98574abddc1e760b57afcffb8bd2f927aa
teresaliu20 <teresali@usc.edu>
2017-09-21 23:22:33 -0700

    Added last photos and menu

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,58 @@
 //set largest circle
 $(document).ready(function() {
     AOS.init();
-
-
+    var openMenu = 0;
+    $('#menu-button').on('click', function() {
+    	if (openMenu === 0) {
+    		$('.nav-bar').css({
+	    		"flex-direction": "column",
+	    	});
+			$('.nav-tab').css({
+	    		"display": "block",
+	    	});
+	    	$('#menu-button').css({
+	    		"width": "100%",
+	    	});
+	    	openMenu = 1;
+    	}
+    	else {
+    		$('.nav-bar').css({
+	    		"flex-direction": "row",
+	    	});
+			$('.nav-tab').css({
+	    		"display": "none",
+	    	});
+	    	$('#menu-button').css({
+	    		"display": "flex",
+	    		"width": "40px"
+	    	});
+	    	openMenu = 0;
+    	}
+    	
+    })
+    $(window).on('resize', function() {
+    	if ($(window).width() > 800) {
+    		console.log("HEe")
+    		$('.nav-bar').css({
+	    		"flex-direction": "row",
+	    	});
+			$('.nav-tab').css({
+	    		"display": "flex",
+	    	});
+	    	$('#menu-button').css({
+	    		"display": "none"
+	    	});
+    	}
+    	else {
+    		$('.nav-bar').css({
+	    		"flex-direction": "column",
+	    	});
+			$('.nav-tab').css({
+	    		"display": "block",
+	    	});
+	    	$('#menu-button').css({
+	    		"width": "100%",
+	    	});
+    	}
+    })
 });
